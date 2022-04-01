252b9d4b6b0e1df8f717d79ad671ab030a6b35bb
guilherme.ricioli <guilherme.riciolic@gmail.com>
2022-04-01 17:22:37 -0300

    Added services bringup to clean script

--- clean.sh
+++ clean.sh
@@ -3,3 +3,5 @@
 TEST_FOLDER=/home/lnls-bpm/repos/test
 
 sudo rm -rf ${TEST_FOLDER}
+sudo systemctl unmask halcs-be-ioc@${BPM_IDX} halcs-be-ioc@${BPM_IDX}
+sudo systemctl start halcs-be-ioc@${BPM_IDX} halcs-be-ioc@${BPM_IDX}
