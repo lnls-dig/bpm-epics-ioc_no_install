e154b4aae03ec4e0fa229322403e01b15c568ed4
guilherme.ricioli <guilherme.riciolic@gmail.com>
2022-04-04 16:26:07 -0300

    Fixed improper services' manipulation

--- run/halcs_run.sh
+++ run/halcs_run.sh
@@ -4,8 +4,11 @@ BPM_IDX=$1
 
 TEST_FOLDER=/home/lnls-bpm/repos/test
 
-sudo systemctl stop halcs-be-ioc@${BPM_IDX}
-sudo systemctl mask halcs-be-ioc@${BPM_IDX}
+sudo systemctl stop halcs-be@${BPM_IDX}.target
+sudo systemctl mask halcs-be@${BPM_IDX}.target
 
 cd ${TEST_FOLDER}/halcs/build/src/apps/halcsd
 ./halcsd -f /etc/halcs/halcs.cfg -n be -t pcie -i ${BPM_IDX} -b ipc:///tmp/malamute -l /tmp/log/halcs
+
+sudo systemctl unmask halcs-be@${BPM_IDX}.target
+sudo systemctl restart halcs-be@${BPM_IDX}.target
