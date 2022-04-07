3e86b32b01cf9242b3833bc8f154833de36fdb1d
guilherme.ricioli <guilherme.riciolic@gmail.com>
2022-04-07 11:09:19 -0300

    Allowed user to choose branch/tag for each repos

--- clone_compile.sh
+++ clone_compile.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-TEST_TAG=test_interlock
+HALCS_BRANCH=$1
+BPM_EPICS_IOC_BRANCH=$2
+
 TEST_FOLDER=/tmp/repos/test
 
 mkdir -p ${TEST_FOLDER}
 cp patches/no-installation-halcs.patch patches/no-installation-bpm-epics-ioc.patch ${TEST_FOLDER}
 
 cd ${TEST_FOLDER}
-git clone --recursive -b ${TEST_TAG} --single-branch https://github.com/lnls-dig/halcs.git
-git clone --recursive -b ${TEST_TAG} --single-branch https://github.com/lnls-dig/bpm-epics-ioc
+git clone --recursive -b ${HALCS_BRANCH} --single-branch https://github.com/lnls-dig/halcs.git
+git clone --recursive -b ${BPM_EPICS_IOC_BRANCH} --single-branch https://github.com/lnls-dig/bpm-epics-ioc
 
 cd ${TEST_FOLDER}/halcs
 git am ../no-installation-halcs.patch
